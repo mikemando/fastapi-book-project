68028cbc8bb43990794a4c02f02be3b4c0f1d088
mikemando <ikwuegbu300@gmail.com>
2025-02-13 10:06:11 +0100

    reorder api routes

--- api/routes/books.py
+++ api/routes/books.py
@@ -40,6 +40,13 @@ async def create_book(book: Book):
         status_code=status.HTTP_201_CREATED, content=book.model_dump()
     )
 
+
+@router.get(
+    "/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK
+)
+async def get_books() -> OrderedDict[int, Book]:
+    return db.get_books()
+
 @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def get_book(book_id:int) -> Book:
     book = db.get_book(book_id)
@@ -48,13 +55,6 @@ async def get_book(book_id:int) -> Book:
         raise HTTPException(status_code=404, detail="Book not found")
 
     return book
-
-@router.get(
-    "/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK
-)
-async def get_books() -> OrderedDict[int, Book]:
-    return db.get_books()
-
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
